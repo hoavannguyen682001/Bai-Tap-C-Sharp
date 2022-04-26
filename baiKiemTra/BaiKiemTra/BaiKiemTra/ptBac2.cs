904bebfa86b42c3719d393d61e6c0607ba35f4dd
hoavannguyen682001 <60832263+hoavannguyen682001@users.noreply.github.com>
2022-04-26 07:34:06 +0700

    commit bai tap diem danh

---
+++ baiKiemTra/BaiKiemTra/BaiKiemTra/ptBac2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiKiemTra
+{
+    public class ptBac2
+    {
+        private double a, b, c;
+
+        public ptBac2()
+        {
+        }
+
+        public ptBac2(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+        public double C { get => c; set => c = value; }
+    }
+}
