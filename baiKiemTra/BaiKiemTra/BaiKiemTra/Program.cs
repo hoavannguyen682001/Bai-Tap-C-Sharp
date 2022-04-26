a6449cf063be4b67eb0af529087d671870104b98
hoavannguyen682001 <60832263+hoavannguyen682001@users.noreply.github.com>
2022-04-26 07:48:02 +0700

    commit bai tap

--- baiKiemTra/BaiKiemTra/BaiKiemTra/Program.cs
+++ baiKiemTra/BaiKiemTra/BaiKiemTra/Program.cs
@@ -10,6 +10,16 @@ namespace BaiKiemTra
     {
         static void Main(string[] args)
         {
+            double a, b, c;
+            Console.WriteLine("Nhap a,b,c");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+
+            ptBac2 pt = new ptBac2();
+            pt.nghiemPT(a, b, c);
+
+            Console.ReadKey();
         }
     }
 }
--- baiKiemTra/BaiKiemTra/BaiKiemTra/ptBac2.cs
+++ baiKiemTra/BaiKiemTra/BaiKiemTra/ptBac2.cs
@@ -24,5 +24,34 @@ namespace BaiKiemTra
         public double A { get => a; set => a = value; }
         public double B { get => b; set => b = value; }
         public double C { get => c; set => c = value; }
+
+        public double delta(double a, double b, double c)
+        { 
+            return (b*b - 4*a*c);
+        }
+
+        public void nghiemPT(double a, double b, double c)
+        {
+            double del = delta(a, b, c);
+            if (a == 0)
+            {
+                Console.WriteLine("Phuong trinh tro thanh bac nhat");
+                Console.WriteLine("Co nghiem bang, x = {0}", -c / b);
+            }
+            else
+            {
+                if (del < 0)
+                    Console.WriteLine("Phuong trinh vo nghiem");
+                else if (del == 0)
+                    Console.WriteLine("Phuong trinh co nghiem kep, x1 = x2 = {0}", -b / (2 * a));
+                else
+                {
+                    Console.WriteLine("Phuong trinh co 2 nghiem phan biet");
+                    Console.WriteLine("x1 = {0}", ((-b + Math.Sqrt(del)) / (2 * a)));
+                    Console.WriteLine("x2 = {0}", ((-b - Math.Sqrt(del)) / (2 * a)));
+                }
+            }
+        }
+            
     }
 }
